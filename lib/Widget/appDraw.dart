98b3b216c651d26878c68e5ba52121395127182e
NishadAvnish <avnishkumar1st@gmail.com>
2020-06-24 16:51:25 +0530

    drawer modified

--- lib/Widget/appDraw.dart
+++ lib/Widget/appDraw.dart
@@ -1,89 +1,115 @@
-
 import 'package:flutter/material.dart';
+import 'package:freshkart/Provider/home_category_provider.dart';
+import 'package:freshkart/Util/color.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
-  const AppDrawer({Key key}) : super(key: key);
-   
+  HomeMainCategoryProvider _homeMainCategoryProvider;
+  List _menu = [
+    "Home",
+    "Search",
+    "Shop by Category",
+    "Your Order",
+    "Cart",
+    "FAQ"
+  ];
+  List icon = [
+    Icons.home,
+    Icons.search,
+    Icons.category,
+    Icons.assignment,
+    Icons.add_shopping_cart,
+    Icons.question_answer
+  ];
+
   @override
   Widget build(BuildContext context) {
-    List _menu = ["Home", "Shop by Category","Your Order","Cart", "Search", "FAQ"  ];
-    return Drawer(
-         child: ListView(
-          // shrinkWrap: true,
-           
-     children: <Widget>[
-       _drawHeader(context),
-       _drawItem(_menu),
-     ],
-
-
-          )
+    _homeMainCategoryProvider = Provider.of<HomeMainCategoryProvider>(context);
+    return Padding(
+      padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+      child: Drawer(
+          elevation: 0.0,
+          child: MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverToBoxAdapter(
+                  child: Container(
+                      width: double.infinity,
+                      color: mainColor,
+                      child: _drawHeader(context)),
+                ),
+                // SliverToBoxAdapter(child: _drawItem(_menu)),
+                SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                  return _drawItem(_menu, index);
+                }, childCount: _menu.length))
+              ],
+            ),
+          )),
     );
   }
 
-Widget _drawHeader( BuildContext context){
-  return DrawerHeader(child: 
- Column(children: <Widget>[ 
-   Expanded(child:  CircleAvatar( radius: 40,child: FlutterLogo(), )),
-   Text("Avnish" , style: Theme.of(context).textTheme.headline6,),
-   Text("9971604962", style: Theme.of(context).textTheme.bodyText2,),
- 
- 
- ],));
-
-  //Container(child: CircleAvatar(child: FlutterLogo(),),));
-  
-}
-//
-Widget _drawItem( List menu ){
-  return ListView.builder(
-    shrinkWrap: true,
-    itemCount: menu.length,
-    itemBuilder: (_,index){
-         
-    
-   if(menu[index] == "Your Order" || menu[index] == "FAQ"){
-        
-           return 
-           ListView(shrinkWrap: true,
-           children: <Widget>[
-             Padding(
-               padding: const EdgeInsets.all(8.0),
-               child: Divider(),
-             ),
-             ListTile(leading: FlutterLogo(), title: Text(menu[index]),),
-
-           ],);
-          //  ListView(
-        
-          //   
-          // );
-      }
-      else if (menu[index] == "Shop by Category" )
-    {
-      return ExpansionTile( leading: FlutterLogo(),
-        title: Text(menu[index]),
+  Widget _drawHeader(BuildContext context) {
+    return Column(
       children: <Widget>[
-        ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-          ListTile(title: Text("ant snaht")),
-          ListTile(title: Text("ant snaht")),
-          ListTile(title: Text("ant snaht")),
-          ListTile(title: Text("ant snaht")),
-          ListTile(title: Text("ant snaht")),
-          ]
-        )
+        SizedBox(height: 5),
+        CircleAvatar(
+          radius: 40,
+          child: FlutterLogo(),
+        ),
+        SizedBox(height: 15),
+        Text(
+          "Avnish",
+          style: Theme.of(context).textTheme.subtitle2,
+        ),
+        Text(
+          "9971604962",
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        SizedBox(height: 5),
       ],
-       );
-      
+    );
+  }
 
+  Widget _drawItem(List menu, int index) {
+    if (menu[index] == "Shop by Category") {
+      return Column(
+        children: <Widget>[
+          ExpansionTile(
+            leading: Icon(icon[index]),
+            title: Text(menu[index]),
+            children: <Widget>[
+              ListView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: _homeMainCategoryProvider.mainCategoryList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                        child: Image.asset(_homeMainCategoryProvider
+                            .mainCategoryList[index].imageUrl)),
+                    title: Text(_homeMainCategoryProvider
+                        .mainCategoryList[index].title),
+                  );
+                },
+              )
+            ],
+          ),
+          Divider()
+        ],
+      );
+    } else {
+      return Column(
+        children: <Widget>[
+          (menu[index] == "FAQ") ? Divider() : Container(),
+          ListTile(
+            leading: Icon(icon[index]),
+            title: Text(menu[index]),
+          ),
+        ],
+      );
     }
-      else
-      return ListTile(leading: FlutterLogo(), title: Text(menu[index]),);
-    });
-}
-
-
-
+  }
 }
